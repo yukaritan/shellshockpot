8d38abcb7c750e0ddd5ccfbe2afd33f6adbb8a1e
yukaritan <yukaritan@users.noreply.github.com>
2014-10-26 12:44:43 +0100

    Experimental: the honeypot can now pretend to be phpmyadmin and wordpress Broke a bunch of code out of main.py and made decorators (that's interception for the C# folks out there)
    Special thanks to Gosha <3

---
+++ src/decorators.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from flask import request
+
+
+def _headers2dict(req: request) -> dict:
+    """Grab the headers out of the request object and turn them into a dict"""
+
+    return dict(req.headers.to_list())
+
+
+def _printdict(data: dict, indentlevel=0, file=None):
+    """A silly little utility to prettily print (nested) dicts"""
+
+    outerindent = '    ' * indentlevel
+    innerindent = '    ' * (indentlevel + 1)
+
+    print(outerindent + '{', file=file)
+
+    for k, v in data.items():
+        if type(v) is dict:
+            print('{innerindent}"{k}":'.format(innerindent=innerindent, k=k), file=file)
+            _printdict(v, indentlevel=indentlevel+1, file=file)
+        else:
+            print('{innerindent}"{k}": "{v}"'.format(innerindent=innerindent, k=k, v=v), file=file)
+
+    print(outerindent + '}', file=file)
+
+
+def _printreport(path, file=None):
+    """I call print a thousand times here. I use it both to write to file and to print to the screen."""
+
+    print("Time:", datetime.now(), file=file)
+    print("Host:", request.remote_addr, file=file)
+    print("Path: /" + path, file=file)
+    _printdict(_headers2dict(request), file=file)
+    print("---", file=file)
+
+
+def log(fn):
+    """Decorate a function with @decorators.log to add logging"""
+    def wrapper(path, *args, **kw):
+        with open('log.txt', 'a+') as logfile:
+            _printreport(path, file=logfile)
+        _printreport(path)
+        return fn(path, *args, **kw)
+
+    wrapper.__name__ = fn.__name__
+    return wrapper
--- src/main.py
+++ src/main.py
@@ -1,69 +1,46 @@
 #!/usr/bin/env python3.4
-from datetime import datetime
 
-from flask import Flask, request
-
+from flask import Flask
 from regexconverter import RegexConverter
-
+import decorators
 
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
 
 
-def headers2dict(req: request) -> dict:
-    """Grab the headers out of the request object and turn them into a dict"""
-
-    return dict(req.headers.to_list())
-
-
-def printdict(data: dict, indentlevel=0, file=None):
-    """A silly little utility to prettily print (nested) dicts"""
-
-    outerindent = '    ' * indentlevel
-    innerindent = '    ' * (indentlevel + 1)
-
-    print(outerindent + '{', file=file)
-
-    for k, v in data.items():
-        if type(v) is dict:
-            print('{innerindent}"{k}":'.format(innerindent=innerindent, k=k), file=file)
-            printdict(v, indentlevel=indentlevel+1, file=file)
-        else:
-            print('{innerindent}"{k}": "{v}"'.format(innerindent=innerindent, k=k, v=v), file=file)
-
-    print(outerindent + '}', file=file)
-
-
-def printreport(path, file=None):
-    """I call print a thousand times here. I use it both to write to file and to print to the screen."""
-
-    print("Time:", datetime.now(), file=file)
-    print("Host:", request.remote_addr, file=file)
-    print("Path: /" + path, file=file)
-    printdict(headers2dict(request), file=file)
-    print("---", file=file)
-
-
 @app.route('/robots.txt')
 def robotstxt():
+    """Tell crawlers to piss off"""
     return open('src/robots.txt', 'r').read()
 
 
 @app.route('/your-detectify-id-here.txt')
 def detectify():
+    """In case you're running detectify"""
     return "detectify"
 
 
+@app.route('/<regex(".*phpMyAdmin.*"):path>')
+@decorators.log
+def phpmyadmin(path: str):
+    """Pretend to be phpMyAdmin"""
+    return open('responses/phpMyAdmin-2.11.11.3.txt', 'r').read()
+
+
+@app.route('/<regex(".*wp-login\.php.*"):path>')
+@decorators.log
+def wordpress(path: str):
+    """Pretend to be wp-login.php"""
+    return open('responses/wordpress.txt', 'r').read()
+
+
 @app.route('/<regex(".*"):path>')
+@decorators.log
 def landing(path: str):
     """With the above regex, this function will be hit no matter what path you try."""
-
-    with open('log.txt', 'a+') as log:
-        printreport(path, file=log)
-    printreport(path)
-
     return 'some response here'  # this is where we stick our bait!
                                  # well, unless we come up with some better regexes.. regexii? regii?
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
